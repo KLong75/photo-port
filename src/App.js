2d689c4b47411baefdc243d94d0f04345824ee9d
Kevin Long <98487770+KLong75@users.noreply.github.com>
2022-06-15 20:07:42 -0500

    create contact form component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
 import Gallery from './components/Gallery';
+import ContactForm from './components/Contact';
 
 
 function App() {
@@ -22,8 +23,10 @@ const [currentCategory, setCurrentCategory] = useState(categories[0]);
         currentCategory = {currentCategory}
       ></Nav>
       <main>
+        <ContactForm></ContactForm>
         <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
+        
       </main>
     </div>
   );
